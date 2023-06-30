b4636d440be2cff0885d1f5b074a68ee1937251e
Govind Thakur <govindthakur25@gmail.com>
2023-06-30 18:32:48 +0530

    Refactor resolvers to have query and session in their separate files

--- resolvers.js
+++ resolvers.js
@@ -1,30 +1,7 @@
-const _ = require("lodash");
+const Query = require("./resolvers/query");
+const Session = require("./resolvers/session");
 
 module.exports = {
-  Query: {
-    sessions: (parent, args, context, info) => {
-      const { dataSources } = context;
-      return dataSources.sessionAPI.getSessions(args);
-    },
-    sessionById: (parent, args, context, info) => {
-      const { id } = args;
-      const { dataSources } = context;
-      return dataSources.sessionAPI.getSessionById(id);
-    },
-    speakers: (parent, args, { dataSources }, info) => {
-      return dataSources.speakerAPI.getSpeakers();
-    },
-    speakerById: (parent, { id }, { dataSources }, info) => {
-      return dataSources.speakerAPI.getSpeakerById(id);
-    },
-  },
-  Session: {
-    async speakers(session, args, { dataSources }) {
-      const speakers = await dataSources.speakerAPI.getSpeakers();
-      const speaker = speakers.filter((speaker) => {
-        return _.filter(session.speakers, { id: speaker.id }).length;
-      });
-      return speaker;
-    },
-  },
+  Query,
+  Session,
 };
