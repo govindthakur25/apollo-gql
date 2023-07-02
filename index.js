2769b7db34f01dfe1cd4e8bb2fddceb0bd514f70
Govind Thakur <govindthakur25@gmail.com>
2023-07-02 17:29:16 +0530

    Add error handling for Speaker API

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, ApolloError } = require("apollo-server");
 const SessionAPI = require("./datasources/sessions");
 const SpeakerAPI = require("./datasources/speakers");
 
@@ -10,7 +10,22 @@ const dataSources = () => ({
   speakerAPI: new SpeakerAPI(),
 });
 
-const server = new ApolloServer({ typeDefs, resolvers, dataSources });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources,
+  debug: false,
+  formatError: (error) => {
+    if (error.extensions.code == "INTERNAL_SERVER_ERROR") {
+      return new ApolloError(
+        "Something went wrong, we're looking into it",
+        "ERROR",
+        { token: "uniquetoken" }
+      );
+    }
+    return error;
+  },
+});
 server.listen({ port: process.env.port || 4000 }).then(({ url }) => {
   console.log({ url });
 });
--- resolvers/session.js
+++ resolvers/session.js
@@ -1,11 +1,20 @@
+const { ApolloError } = require("apollo-server");
 const _ = require("lodash");
 
 module.exports = {
   async speakers(session, args, { dataSources }) {
-    const speakers = await dataSources.speakerAPI.getSpeakers();
-    const speaker = speakers.filter((speaker) => {
-      return _.filter(session.speakers, { id: speaker.id }).length;
-    });
-    return speaker;
+    try {
+      const speakers = await dataSources.speakerAPI.getSpeakers();
+      const speaker = speakers.filter((speaker) => {
+        return _.filter(session.speakers, { id: speaker.id }).length;
+      });
+      return speaker;
+    } catch (error) {
+      return new ApolloError(
+        "Cannot communicate with the Speaker API at this moment",
+        "SPEAKER_API_ERROR",
+        { token: "UNIQUE_TOKEN" }
+      );
+    }
   },
 };
